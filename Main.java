7421f3d0d3166a8d6b5fab91aa80978698f0063c
Amila Weerasinghe <40320449+AmilaWeerasinghe@users.noreply.github.com>
2019-02-24 21:40:13 +0530

    Update Main.java
    Fully working Main class.
    Thread joining has to set.

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
-
+package ThreadMul;
 
 class Main { 
     public static int [][] a = {{1, 1, 1},
 				{1, 1, 1},
 				{1, 1, 1}};
     
-    public static int [][] b = {{1 },
-				{1 },
-				{1 }};
+    public static int [][] b = {{1 ,1},
+				{1,1 },
+				{1 ,1}};
+          public static int Nrows=a.length;
+          public static int Ncols=b[0].length;
 
     public static void print_matrix(int [][] a) {
 	for(int i=0; i < a.length; i++) {
@@ -19,11 +21,16 @@ class Main {
     
 
     public static void main(String [] args) { 
-    	//start measure the time
-    	long s = System.currentTimeMillis();
-	int [][] x = Matrix.multiply(a, b); 
-	print_matrix(x); // see if the multipication is correct 	
-	long e = System.currentTimeMillis();
-    System.out.println("Time to execute in milliseconds = "+(e-s));
-    }
+
+	
+	for(int i=0;i<a.length;i++){
+	Thread thread=new Thread(new Matrix(a[i],b,i));
+	thread.start();
+	}
+	
+	
+	//System.out.println(Matrix.c.length);
+print_matrix(Matrix.c);
+	
+	}
 }
